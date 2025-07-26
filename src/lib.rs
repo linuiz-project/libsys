64635dd5b5d7ecee86e9775dd2a2161d949b4e37
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-07-26 00:30:28 -0500

    add comment & tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
+#![feature(step_trait)]
 
-// TODO account for pointer width not matching register width
+// In the future, there may be a platform where the pointer width does not
+// exactly match CPU's the register width. In that case, we may need to
+// introduce special types like `uptr` or `ureg` to handle the differing
+// sizes of the CPU-internal structures.
 
 mod address;
 pub use address::*;
@@ -13,20 +17,57 @@ pub mod syscall;
 #[macro_use]
 extern crate static_assertions;
 
-use core::num::NonZeroU32;
+use core::{num::NonZero, ops::Shr};
 
-pub const fn align_up(value: usize, alignment_bits: NonZeroU32) -> usize {
+pub const fn align_up(value: usize, alignment_bits: NonZero<u32>) -> usize {
     (value.wrapping_neg() & (1usize << alignment_bits.get()).wrapping_neg()).wrapping_neg()
 }
 
-pub const fn align_up_div(value: usize, alignment_bits: NonZeroU32) -> usize {
+pub const fn align_up_div(value: usize, alignment_bits: NonZero<u32>) -> usize {
     align_up(value, alignment_bits) / (1usize << alignment_bits.get())
 }
 
-pub const fn align_down(value: usize, alignment_bits: NonZeroU32) -> usize {
-    value & !((1usize << alignment_bits.get()) - 1)
+pub const fn align_down(value: usize, alignment_bits: NonZero<u32>) -> usize {
+    (value >> alignment_bits.get()) << alignment_bits.get()
 }
 
-pub const fn align_down_div(value: usize, alignment_bits: NonZeroU32) -> usize {
-    align_down(value, alignment_bits) / (1usize << alignment_bits.get())
+pub const fn align_down_div(value: usize, alignment_bits: NonZero<u32>) -> usize {
+    value >> alignment_bits.get()
+}
+
+#[cfg(test)]
+mod tests {
+    use core::num::NonZero;
+
+    #[test]
+    pub fn align_up() {
+        assert_eq!(
+            super::align_up(0xFC, unsafe { NonZero::new_unchecked(4) }),
+            0x100
+        );
+    }
+
+    #[test]
+    pub fn align_up_div() {
+        assert_eq!(
+            super::align_up_div(0xFC, unsafe { NonZero::new_unchecked(4) }),
+            0x10
+        );
+    }
+
+    #[test]
+    pub fn align_down() {
+        assert_eq!(
+            super::align_down(0xFC, unsafe { NonZero::new_unchecked(4) }),
+            0xF0
+        );
+    }
+
+    #[test]
+    pub fn align_down_div() {
+        assert_eq!(
+            super::align_down_div(0xFC, unsafe { NonZero::new_unchecked(4) }),
+            0xF
+        );
+    }
 }
